dd54e7926cc3dcb4430bbc2571606a025f979332
fyui001 <yun.cod@gmail.com>
2020-03-17 01:31:57 +0900

    リファクタリング

--- index.php
+++ index.php
@@ -1,41 +1,40 @@
 <?php
-
 /* ソート用関数 */
-function ArrSort($a, $b){
+function descendingSort($a, $b){
     $a = intval($a);
     $b = intval($b);
     return $a - $b;
 }
 
-$FilePath = "./q_5000000_3000.txt"; /* テキストファイルのパス */
-$NumArr = []; /* テキストファイルから取得した文字列を入れる配列 */
-$CountArr = []; /* 重複数が２回の数列を入れる配列 */
-$count = 0; /* 要素数のカウントを入れる変数 */
+$filePath = './q_5000000_3000.txt';
+$numArr = [];
+$duplicationNumArr = [];
 
-if(!file_exists($FilePath)){
+if(!file_exists($filePath)){
     echo "ファイルが見つかりません。\n";
-}else{
-
-    $f = fopen("{$FilePath}", 'r');
-    while(($content = fgets($f, 4096)) !== false){
-        if(isset($content) && isset($NumArr[$content])){
-            $NumArr[$content]++;
-        }else{
-            $NumArr[$content] = 1;
-        }
-    }
-
-    foreach($NumArr as $content => $value){
-        if($value == 2){
-            $CountArr[$content] = 0;
-        }
-    }
-    uksort($CountArr, "ArrSort"); /* 昇順にソート */
-    foreach ($CountArr as $content => $value){
-        echo $content; /* 2回重複している数列のみ表示 */
-    }
-
-    echo "要素数:".count($CountArr)."\n";
-
-    fclose($f);
+    return 0;
 }
+
+$f = fopen($filePath, 'r');
+while( $content = fgets($f, 4096) ) {
+    if( !isset($numArr[$content]) ){
+        $numArr[$content] = 1;
+        continue;
+    }
+    $numArr[$content]++;
+}
+
+foreach($numArr as $content => $value){
+    if($value === 2){
+        $duplicationNumArr[$content] = 0;
+    }
+}
+
+uksort($duplicationNumArr, 'descendingSort');
+foreach ($duplicationNumArr as $content => $value){
+    echo $content;
+}
+
+echo '要素数:'.count($duplicationNumArr)."\n";
+
+fclose($f);
